eeccec3d76c31e0a181c3bd05b1a9c51b234fa8a
Jacob McSwain <jacob@clevyr.com>
2018-11-11 14:37:48 -0600

    Add google analytics

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { createBrowserHistory } from "history";
 import { Router, Route, Switch } from "react-router-dom";
 
 import "assets/css/material-dashboard-react.css?v=1.5.0";
+import GAListener from "components/GAListener"
 
 import indexRoutes from "routes/index.jsx";
 
@@ -11,11 +12,13 @@ const hist = createBrowserHistory();
 
 ReactDOM.render(
   <Router history={hist}>
-    <Switch>
-      {indexRoutes.map((prop, key) => {
-        return <Route path={prop.path} component={prop.component} key={key} />;
-      })}
-    </Switch>
+    <GAListener>
+      <Switch>
+        {indexRoutes.map((prop, key) => {
+          return <Route path={prop.path} component={prop.component} key={key} />;
+        })}
+      </Switch>
+    </GAListener>
   </Router>,
   document.getElementById("root")
 );
