4be0edca9702123b66517ca58fbe1aec011dd46b
Jacob McSwain <jacob@clevyr.com>
2018-11-11 22:11:56 -0600

    Make team cards sized correctly

--- src/views/Team/Team.jsx
+++ src/views/Team/Team.jsx
@@ -2,10 +2,11 @@ import React from "react";
 
 import GridItem from "components/Grid/GridItem.jsx";
 import GridContainer from "components/Grid/GridContainer.jsx";
-import Button from "components/CustomButtons/Button.jsx";
+import Grid from "@material-ui/core/Grid/Grid";
 import Card from "components/Card/Card.jsx";
 import CardAvatar from "components/Card/CardAvatar.jsx";
 import CardBody from "components/Card/CardBody.jsx";
+import { Button } from "@material-ui/core";
 
 class Team extends React.Component {
 
@@ -33,8 +34,8 @@ class Team extends React.Component {
         {this.state.teamJson != null &&
           this.state.teamJson.map((prop, key) => {
             return (
-              <GridItem xs={12} sm={12} md={4}>
-                <Card profile>
+              <GridItem xs={12} sm={12} md={4} style={{"margin-bottom": "80px"}}>
+                <Card profile style={{height: "100%", "margin-bottom": "0px"}}>
                   <CardAvatar profile>
                     <img
                       alt={
@@ -44,17 +45,35 @@ class Team extends React.Component {
                         `https://raw.githubusercontent.com/InvictrixRom/team/inv-9.0/avatar/${prop.avatar}`
                       } />
                   </CardAvatar>
-                  <CardBody profile>
-                    <p>
-                      <h3>{prop.name}</h3>
-                      <h4>{prop.xdaUsername}</h4>
+                  <CardBody profile style={{"margin-top": "0px"}}>
+                    <p style={{"margin": "0px"}}>
+                      <h3 style={{"margin": "0px"}}>
+                        {prop.name}
+                        <br />
+                        <small>{prop.xdaUsername}</small>
+                      </h3>
                     </p>
                     <p>
                       {prop.duties}
                     </p>
-                    <Button color="primary" round>
-                      Follow
-                    </Button>
+                    <div>
+                      {prop.devices !== null && prop.devices[0] !== "TBD" && prop.devices[0] !== "" && (<div><hr /><h4>Devices</h4></div>)}
+                      {prop.devices.map((prop, key) => {
+                        if(prop !== "" && prop !== "TBD")
+                          return (
+                            <Grid container justify="center">
+                              <Card style={{width: "50%", margin: "2px"}}>
+                                <a href={`https://get.invictrixrom.com/${prop}`}>
+                                  <Button style={{color: "#ccc", width: "100%"}}>
+                                    {prop}
+                                  </Button>
+                                </a>
+                              </Card>
+                            </Grid>
+                          )
+                        else return null;
+                      })}
+                    </div>
                   </CardBody>
                 </Card>
               </GridItem>
